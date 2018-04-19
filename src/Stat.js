05f51755afbe2ec04db442583b8b5c6a97e467e1
FusionGoat <33003328+FusionGoat@users.noreply.github.com>
2018-04-19 04:09:29 +0300

    0.0.2
    It's aliive!

--- src/Stat.js
+++ src/Stat.js
@@ -4,20 +4,19 @@ import React, { Component } from 'react';
 
 import Paper from 'material-ui/Paper';
 
-
 class Stat extends Component {
   constructor(){
     super();
     this.state = {
       error: null,
       isLoaded: false,
-      data: []
+      data: {}
     };
 
   }
   componentDidMount() {
     const url = 'https://bms.it-tv.org/stat/api.php'
-    let action = "get_messages";
+    let action = "get_abonent";
     fetch(url, {
          method: 'POST',
          headers: {
@@ -67,18 +66,18 @@ const { error, isLoaded, data } = this.state;
           <div>
           <AppBar title="Личный кабинет"/>
           <p>
-
-              {data.map(item => (
+{data.Name}
+              {/* {data(item => (
                 <Paper style={style} zDepth={3}  rounded= {true} key={item.Id}>
-                  <strong>{item.Time}</strong>
-                  <p>{item.Text}</p>
+                  <strong>{item.Name}</strong>
+                  <p>{item.Phones}</p>
                 </Paper>
-              ))}
+              ))} */}
 
 
 
           </p>
-
+{data.Phones}
          </div>
          </MuiThemeProvider>
       </div>
@@ -87,3 +86,86 @@ const { error, isLoaded, data } = this.state;
 }}
 
 export default Stat;
+
+// class Stat extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       error: null,
+//       isLoaded: false,
+//       data: []
+//     };
+//
+//   }
+//   componentDidMount() {
+//     const url = 'https://bms.it-tv.org/stat/api.php'
+//     let action = "get_messages";
+//     fetch(url, {
+//          method: 'POST',
+//          headers: {
+//            "Content-type": "application/x-www-form-urlencoded"
+//          },
+//          body: "action="+action+"&session="+ localStorage.getItem('sessionNumber')
+//        })
+//       .then(res => res.json())
+//       .then(
+//         (result) => {
+//           this.setState({
+//             isLoaded: true,
+//             data: result.data
+//           });
+//         },
+//         // Note: it's important to handle errors here
+//         // instead of a catch() block so that we don't swallow
+//         // exceptions from actual bugs in components.
+//         (error) => {
+//           this.setState({
+//             isLoaded: true,
+//             error
+//           });
+//         }
+//       )
+//   }
+//
+//
+// render() {
+//   const style = {
+//
+//     padding: 50,
+//     margin: 20,
+//     textAlign: 'center',
+//     display: 'inline-block',
+//   };
+//
+// const { error, isLoaded, data } = this.state;
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   } else if (!isLoaded) {
+//     return <div>Loading...</div>;
+//   } else {
+//    return (
+//       <div>
+//         <MuiThemeProvider>
+//           <div>
+//           <AppBar title="Личный кабинет"/>
+//           <p>
+//
+//               {data.map(item => (
+//                 <Paper style={style} zDepth={3}  rounded= {true} key={item.Id}>
+//                   <strong>{item.Time}</strong>
+//                   <p>{item.Text}</p>
+//                 </Paper>
+//               ))}
+//
+//
+//
+//           </p>
+//
+//          </div>
+//          </MuiThemeProvider>
+//       </div>
+//     );
+//   }
+// }}
+//
+// export default Stat;
